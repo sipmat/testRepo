180fb2ddf7580c8f539288aeddc449fcee5b19c9
sipmat <sip.matej@gmail.com>
2013-11-16 16:59:15 +0000

    Maximal drawup added

--- Portfolio/Stock.py
+++ Portfolio/Stock.py
@@ -4,19 +4,30 @@ class Stock(object):
 		self.name = name
 		self.prices = prices
 		self.MDD = 0
+		self.MDU = 0
 
 	def maximal_drawdown(self):
-		DD = list()
+		DD = 0
 		peak = -99999
 		for price in self.prices:
 			if(price > peak):
 				peak = price
-  			DD.append(100.0 * (peak - price) / peak)
-  			if (DD[len(DD) - 1] > self.MDD):
-    				self.MDD = DD[len(DD) - 1]
+  			DD = (100.0 * (peak - price) / peak)
+  			if (DD > self.MDD):
+    				self.MDD = DD
 		print self.MDD
 
-	#def maximal_drawup(self):
+	def maximal_drawup(self):
+		DU = 0
+                bottom = 99999
+                for price in self.prices:
+                        if(price < bottom):
+                                bottom = price
+                        DU = (100.0 * (price - bottom) / price)
+                        if (DU > self.MDU):
+                                self.MDU = DU
+                print self.MDU
 
 Test1 = Stock("SMTH", [2, 3, 5, 7, 3, 4, 1, 4])
 Test1.maximal_drawdown()
+Test1.maximal_drawup()
